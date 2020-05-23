43eb5c52131f2a46b13a29bfea804e0ec78423e0
damithn <damithnsamarakoon@gmail.com>
2020-05-24 03:25:28 +0530

    Authorization server initial commit

---
+++ src/main/java/com/damithtech/oauth/athorizationserver/Athorizationserver/AthorizationserverApplication.java
@@ -0,0 +1,13 @@
+package com.damithtech.oauth.athorizationserver.Athorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthorizationserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthorizationserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damithtech/oauth/athorizationserver/Athorizationserver/AthorizationserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damithtech.oauth.athorizationserver.Athorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthorizationserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
